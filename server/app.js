a8fa6253e6fceec5d7fedc48bae0860bb9801621
Rick Michael <rickmikegull@icloud.com>
2020-11-26 18:31:37 -0800

    touch ups

--- server/app.js
+++ server/app.js
@@ -4,9 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var indexRouter = require('./routes');
 
 var app = express();
+// serve static files
+// app.use(express.static(path.join(__dirname, 'build')));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
